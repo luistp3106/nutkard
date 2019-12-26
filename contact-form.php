7a96d4e220052d7a5da6da8aa6776dc2d01688ae
Luis Alejandro Trinidad Pichardo <luistp3106@hotmail.com>
2019-12-26 00:37:01 -0400

    commit

--- contact-form.php
+++ contact-form.php
@@ -1,16 +1,15 @@
 <?php
 
 if(isset($_POST['submit'])){
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $message=$_POST['message'];
 
-$name=$_POST['name'];
-$email=$_POST['email'];
-$message=$_POST['message'];
-
-$mailto="luistp3106@hotmail.com";
-$headers="Correo de Nutkard.com:".$email;
-$txt="Has recibido un correo de".$name;".\n\n".$message;
-mail($mailto,$headers,$txt);
-header("Location:contactos.html?mailsend")
+    $mailto="luistp3106@hotmail.com";
+    $headers="Correo de Nutkard.com:".$email;
+    $txt="Has recibido un correo de".$name;".\n\n".$message;
+    mail($mailto,$headers,$txt);
+    header("Location:contactos.php?mailsend");
 }
 
 ?>
